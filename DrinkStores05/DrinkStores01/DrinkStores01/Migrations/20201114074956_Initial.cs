d38dcad2e42e46b3d72c9a6b04ac17d4ea506dd5
Anh Nguyen <anhmanchester2k1@gmail.com>
2020-11-27 20:21:29 +0700

    Lad05

---
+++ DrinkStores05/DrinkStores01/DrinkStores01/Migrations/20201114074956_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DrinkStores01.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Img = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Discount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ DrinkStores05/DrinkStores01/DrinkStores01/Models/DrinkStroreDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrinkStores01.Models
+{
+    public class DrinkStroreDBContext : DbContext
+    {
+        public DrinkStroreDBContext(DbContextOptions<DrinkStroreDBContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
